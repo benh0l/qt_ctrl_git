8846ec323292b82a09171ad6bfbf49a31dcf972b
Alexis Scheuer <Alexis.Scheuer@loria.fr>
2019-05-03 18:04:37 +0200

    Correction de CHSI (pensez à relire avant le commit ;)

--- include/model/CHSI.hpp
+++ include/model/CHSI.hpp
@@ -28,15 +28,15 @@ public:
   /// @param secondPt     the second point, 
   /// @param secondDeriv  the derivative at the second point.
   CubicHermineSplineInterpolation
-  (const point& firstPt, const vector& firstDeriv,
-   const point& secondPt, const vector& secondDeriv);
+  (const Point& firstPt, const Vector& firstDeriv,
+   const Point& secondPt, const Vector& secondDeriv);
 
   /// @brief Gets a point at given parameter on the interpolation.
   /// @param u  the value of the parameter for which a point
   ///           is required.
   /// @return   the point on the interpolation corresponding 
   ///           to the given value of the parameter.
-  point operator[](const double& u) const;
+  Point operator[](const double& u) const;
 
   /// @brief Gets a point at given parameter on the interpolation.
   /// @param u  the value of the parameter for which a point
@@ -44,7 +44,7 @@ public:
   /// @return   the point on the interpolation corresponding 
   ///           to the given value of the parameter.
   /// @see operator[]
-  point pointAt(const double& u) const { return operator[](u); }
+  Point pointAt(const double& u) const { return operator[](u); }
   
   /// @brief Gets the derivative of the interpolation at a point 
   ///        corresponding to a given parameter.
@@ -52,19 +52,19 @@ public:
   ///           is required.
   /// @return   the derivative of the interpolation at the point 
   ///           corresponding to the given value of the parameter.
-  vector derivativeAt(const double& u) const;
+  Vector derivativeAt(const double& u) const;
   
   /// @brief Gets the point along the interpolation which is 
   ///        the nearest from a given point.
   /// @param P  the point from which a nearest one is searched.
   /// @return   the point along the interpolation nearest from P.
-  point nearestFrom(const point& P) const;
+  Point nearestFrom(const Point& P) const;
   
   /// @brief Gets the distance from a point to the interpolation.
   /// @param P  the point from which distance is searched.
   /// @return   the distance between P and the interpolation.
-  /// @see nearestFrom, point::distance
-  double distance(const point& P) const
+  /// @see nearestFrom, Point::distance
+  double distance(const Point& P) const
   { return nearestFrom(P).distance(P); }
   
 }; // end of class CubicHermineSplineInterpolation
